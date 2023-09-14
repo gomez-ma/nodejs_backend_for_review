77da4ad25fb21bb7d701cf12009120c27023d5be
gomez-ma <gomes.ma@northbkk.ac.th>
2023-09-14 17:23:18 +0700

    for review

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const app = express();
 const PORT = 3000;
 const db = require('./config/db.js');
 
-const UserModel = require('./models/user.model.js');
+//const UserModel = require('./models/user.model.js');
 const UserRoute = require('./routes/user.routes.js');
 
 app.use(express.json());
-
 app.use(express.urlencoded({extended: true}));
 
 // http://localhost:3000
